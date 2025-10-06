c45d25c3e8c782630c87a9fb90e4adae7fc6828e
Eduardo Tenoch <eduardotenoch2500@gmail.com>
2025-10-06 12:50:32 -0600

    feat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './context/AuthContext';
 import { Navigation } from './routes';
+import './App.css';
 
 function App() {
 
--- src/layouts/landingLayout/LandingLayout.jsx
+++ src/layouts/landingLayout/LandingLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Footer } from '../../components/landingPage/footer/Footer';
 import { NavBar } from '../../components/landingPage/navBar/NavBar';
+import { Footer } from '../../components/landing/footer';
 
 
 export function LandingLayout(props) {
