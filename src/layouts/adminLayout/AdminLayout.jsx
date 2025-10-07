2805cd66eff613150243e0154436fa057cfd8e44
Eldavi5 <davidsilvabello25@gmail.com>
2025-10-06 18:17:58 -0600

    feat/Manejo de rutas

--- src/layouts/adminLayout/AdminLayout.jsx
+++ src/layouts/adminLayout/AdminLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { useLocation, Navigate } from 'react-router-dom';
 import {
     HeaderAdmin,
     LeftMenu,
@@ -10,8 +11,27 @@ import './AdminLayout.css';
 
 export function AdminLayout(props) {
     const { children } = props;
-    const { auth } = useAuth()
+    const { auth } = useAuth();
+    const { pathname } = useLocation();
+
     if (!auth || auth?.detail) return <LoginLayout />
+
+    console.log(`Ruta actual: ${pathname}, Tipo de Login: ${auth.typeLogin}`);
+
+    const isAdminF1Route = pathname.startsWith('/admin/f1');
+
+    if (auth.typeLogin === 1) {
+        if (isAdminF1Route) {
+            return <Navigate to="/admin" />;
+        }
+    }
+
+    if (auth.typeLogin === 2) {
+        if (!isAdminF1Route) {
+            return <Navigate to="/admin/f1/" />;
+        }
+    }
+
     return (
         <Container fluid className="admin-layout">
             <Row>
