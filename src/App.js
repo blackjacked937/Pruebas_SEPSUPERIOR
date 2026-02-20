1f5ebfe4b1a056e1707f76368a69142735fa6ac2
Gerardo Amador <gergames11@gmail.com>
2026-02-19 20:43:18 -0600

    add: Libreria para gestion de alertas y configuracion en app.js para su uso en toda la web

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { AuthProvider } from './context/AuthContext';
 import { Navigation } from './routes';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 
 function App() {
@@ -9,7 +11,19 @@ function App() {
   return (
     <AuthProvider>
       <Navigation />
+      <ToastContainer
+        position="top-right"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+        style={{ marginTop: "70px" }}
+      />
     </AuthProvider>
+    
   );
 }
 
